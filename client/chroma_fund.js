9b56a9385d1b55df0a3d8faad7ad4851583c71c2
adam wong <adamwong246@gmail.com>
2015-02-16 17:42:35 -0800

    add basic form

--- client/chroma_fund.js
+++ client/chroma_fund.js
@@ -5,4 +5,20 @@ Deps.autorun(function() {
     return Campaigns.find({}).fetch();
   };
 
+  Template.new_campaign.events({
+    'submit form.new_campaign': function(event) {
+      // amount = parseInt(event.target.elements.namedItem('amount').value);
+      // Payments.insert({
+      //   payer: Meteor.userId(),
+      //   amount: amount
+      // });
+      // var cfi = Cfinstances.findOne();
+      // Cfinstances.update({"_id": cfi._id}, {"total_contributions": cfi.total_contributions + amount});
+
+      alert("Your campaign is being created. Look for an email for validation!");
+
+      return false;
+    }
+  });
+
 });
